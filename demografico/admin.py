5ee5c498b366b1c34b4e9f1690a5054fffd520d0
Byron Corrales <zerocool@zerocool-laptop.(none)>
2009-07-06 19:55:45 -0600

    cachimbo de cosas

--- demografico/admin.py
+++ demografico/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from demografico.models import Poblacion
 
-admin.site.register(Poblacion)
+class PoblacionAdmin(admin.ModelAdmin):
+	list_display = ['ano','departamento','total_ambos_sexos', 'total_hombre', 'total_mujer', 'urbano_ambos_sexos', 'rural_ambos_sexos']
+	list_filter = ['ano']
+#	search_fields = ['nombre_productor']
+
+admin.site.register(Poblacion, PoblacionAdmin)
 
--- lugar/admin.py
+++ lugar/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from lugar.models import Departamento, Municipio
 
-admin.site.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+	list_display = ['numero','nombre']
+        list_filter = ['nombre']
+        search_fields = ['nombre']
+
+
+admin.site.register(Departamento,DepartamentoAdmin)
 admin.site.register(Municipio)
 
 
