aaa18a87c19be7f67fefce8efb6455233978f67d
Byron Corrales <zerocool@zerocool-laptop.(none)>
2009-08-14 17:21:04 -0600

    Corrigiendo los modelos de la app lugar

--- lugar/models.py
+++ lugar/models.py
@@ -3,18 +3,23 @@
 from django.db import models
 
 class Departamento(models.Model):
+	numero = models.IntegerField(max_length=10,primary_key=True)
 	nombre = models.CharField(max_length=30)
-	numero = models.DecimalField(decimal_places=0,max_digits=2,primary_key=True)			
+	extension = models.DecimalField("Extension Territorial", max_digits=10,decimal_places=2)
 	
-        class Meta:
-            verbose_name_plural = "Departamentos"
+	
+	class Meta:
+		verbose_name_plural = "Departamentos"
 
 	def __unicode__(self):
-             return self.nombre
-        
+		return self.nombre
+
 class Municipio(models.Model):
-	nombre = models.CharField(max_length=30)		
+	numero = models.IntegerField(max_length=10,primary_key=True)
 	departamento = models.ForeignKey(Departamento)
+	nombre = models.CharField(max_length=30)
+			
+	
         
         class Meta:
             verbose_name_plural = "Municipios"
