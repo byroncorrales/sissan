13b85328cf9a72825cdc4944a3f1197d7ff6d842
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2009-09-29 11:08:13 -0600

    removing .pyc, fixing lugar app, fixing settings

--- demografico/urls.py
+++ demografico/urls.py
@@ -4,6 +4,5 @@ from models import Poblacion
 urlpatterns = patterns('demografico.views',
     (r'^poblacion/consulta-ano/$', 'consulta_ano'),
     (r'^poblacion/$', 'poblacion'),
-    (r'^poblacion/consulta-rango/$', 'consulta_rango'),
  
 )
--- settings.py
+++ settings.py
@@ -1,38 +1,4 @@
-# Django settings for sissan project.
-import os
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-#def rel(*x):
-#         return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-        
-
-#MEDIA_ROOT = PROJECT_DIR + '/media'
-
-ADMINS = (
-     ('Byron Corrales', 'byroncorrales@gmail.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'sissan'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'root'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'es-ni'
+from settings_local import *
 
 SITE_ID = 1
 
@@ -89,8 +55,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django_evolution',
-    'demografico',
+    #'demografico',
     'lugar',
-    'economico',
-    'socio_demografico',
+    #'economico',
+    #'socio_demografico',
 )
