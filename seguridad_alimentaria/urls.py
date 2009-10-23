952380b284889d36b764ecb8307233b8978b4836
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2009-10-23 11:20:49 -0600

    fixing bugs in seguridad alimentaria

--- seguridad_alimentaria/urls.py
+++ seguridad_alimentaria/urls.py
@@ -5,24 +5,24 @@ urlpatterns = patterns('seguridad_alimentaria.views',
                         (r'^$', 'index'),
                         #utilizacion biologica
                         (r'^utilizacion-biologica/$', 'utilizacion_biologica'),
-                        (r'^utilizacion-biologica/(?P<departamento>\w+)/$', 'utilizacion_biologica'),
-                        (r'^utilizacion-biologica/(?P<departamento>\w+)/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/$', 'utilizacion_biologica'),
                         (r'^utilizacion-biologica/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/$', 'utilizacion_biologica'),
                         (r'^utilizacion-biologica/(?P<ano_inicial>\d{4})/$', 'utilizacion_biologica'),
+                        (r'^utilizacion-biologica/(?P<departamento>\w+)/$', 'utilizacion_biologica'),
+                        (r'^utilizacion-biologica/(?P<departamento>\w+)/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/$', 'utilizacion_biologica'),
                         #dependencia alimentaria
                         (r'^dependencia-alimentaria/$', 'dependencia_alimentaria'),
                         (r'^dependencia-alimentaria/(?P<ano_inicial>\d{4})/$', 'dependencia_alimentaria'),
                         (r'^dependencia-alimentaria/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/$', 'dependencia_alimentaria'),
                         #dependencia alimentaria por producto
-                        (r'^dependencia-alimentaria/(?P<producto>\w+)/$', 'dependencia_alimentaria_producto'),
                         (r'^dependencia-alimentaria/(?P<ano_inicial>\d{4})/(?P<producto>\w+)/$', 'dependencia_alimentaria_producto'),
                         (r'^dependencia-alimentaria/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/(?P<producto>\w+)/$', 'dependencia_alimentaria_producto'),
+                        (r'^dependencia-alimentaria/(?P<producto>\w+)/$', 'dependencia_alimentaria_producto'),
                         #disponibilidad
                         (r'^disponibilidad/$', 'disponibilidad'),
                         (r'^disponibilidad/(?P<ano_inicial>\d{4})/$', 'disponibilidad'),
                         (r'^disponibilidad/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/$', 'disponibilidad'),
-                        (r'^disponibilidad/(?P<producto>\w+)/$', 'disponibilidad'),
                         (r'^disponibilidad/(?P<ano_inicial>\d{4})/(?P<producto>\w+)/$', 'disponibilidad'),
                         (r'^disponibilidad/(?P<ano_inicial>\d{4})-(?P<ano_final>\d{4})/(?P<producto>\w+)/$', 'disponibilidad'),
+                        (r'^disponibilidad/(?P<producto>\w+)/$', 'disponibilidad'),
 
                        )
--- seguridad_alimentaria/views.py
+++ seguridad_alimentaria/views.py
@@ -30,23 +30,34 @@ def dependencia_alimentaria(request, ano_inicial=None, ano_final=None):
                 fila['datos'].append(0)
         resultados.append(fila)
     else:
-        limites = DependenciaAlimentaria.objects.all().aggregate(maximo=Max('ano'), minimo=Min('ano'))
-        for ano in range(limites['minimo'], limites['maximo']+1):
-            fila = {'ano': ano, 'datos': []}
-            for producto in productos:
-                try:
-                    dato = DependenciaAlimentaria.objects.get(ano=ano, producto=producto)
-                    fila['datos'].append(dato.dependencia_alimentaria)
-                except:
-                    fila['datos'].append(0)
-            resultados.append(fila) 
+        try:
+            limites = DependenciaAlimentaria.objects.all().aggregate(maximo=Max('ano'), minimo=Min('ano'))
+            for ano in range(limites['minimo'], limites['maximo']+1):
+                fila = {'ano': ano, 'datos': []}
+                for producto in productos:
+                    try:
+                        dato = DependenciaAlimentaria.objects.get(ano=ano, producto=producto)
+                        fila['datos'].append(dato.dependencia_alimentaria)
+                    except:
+                        fila['datos'].append(0)
+                resultados.append(fila) 
+        except:
+            pass
 
     variaciones = []
-    tope = len(resultados) - 1 
-    fila_inicial = resultados[0]['datos']
-    fila_final = resultados[tope]['datos']
+    try:
+        tope = len(resultados) - 1 
+        fila_inicial = resultados[0]['datos']
+        fila_final = resultados[tope]['datos']
+    except:
+        tope = 0
+        fila_inicial = None
+        fila_final = None
     for i in range(tope):
-        variacion = ((fila_final[i]-fila_inicial[i])/fila_inicial[i])*100 if fila_inicial[i]!=0 else 100 
+        try:
+            variacion = ((fila_final[i]-fila_inicial[i])/fila_inicial[i])*100 if fila_inicial[i]!=0 else 100 
+        except:
+            variacion = 0
         variaciones.append("%.2f" % variacion) 
 
     dicc = {'resultados': resultados, 'variaciones': variaciones, 
@@ -100,7 +111,7 @@ def dependencia_alimentaria_producto(request, producto, ano_inicial=None, ano_fi
 def utilizacion_biologica(request, ano_inicial=None, ano_final=None, departamento=None):
     if departamento:
         tiene_dep=True
-        nombre_dep = Departamento.objects.get(slug=departamento).nombre
+        nombre_dep = get_object_or_404(Departamento, slug=departamento).nombre
         if ano_inicial and ano_final:
             datos = UtilizacionBiologica.objects.filter(ano__range=(ano_inicial, ano_final), departamento__slug =departamento)
             mensaje = "Utilizacion Biologica departamento de %s (%s-%s)" % (nombre_dep, ano_inicial, ano_final)
@@ -132,19 +143,26 @@ def utilizacion_biologica(request, ano_inicial=None, ano_final=None, departament
             datos.append(resultado)
             mensaje = "Utilizacion Biologica (%s)" % (ano_inicial)
         else:
-            anos = UtilizacionBiologica.objects.all().aggregate(ano_minimo = Min('ano'), ano_maximo= Max('ano'))
-            for ano in range(anos['ano_minimo'], anos['ano_maximo']+1):
-                resultado = UtilizacionBiologica.objects.filter(ano=ano).aggregate(
-                    enfermedades_diarreicas=Sum('enfermedades_diarreicas'), 
-                    enfermedades_respiratorias=Sum('enfermedades_respiratorias'))
-                resultado['ano']=ano
-                datos.append(resultado)
-            mensaje = "Utilizacion Biologica" 
+            try:
+                anos = UtilizacionBiologica.objects.all().aggregate(ano_minimo = Min('ano'), ano_maximo= Max('ano'))
+                for ano in range(anos['ano_minimo'], anos['ano_maximo']+1):
+                    resultado = UtilizacionBiologica.objects.filter(ano=ano).aggregate(
+                        enfermedades_diarreicas=Sum('enfermedades_diarreicas'), 
+                        enfermedades_respiratorias=Sum('enfermedades_respiratorias'))
+                    resultado['ano']=ano
+                    datos.append(resultado)
+                mensaje = "Utilizacion Biologica" 
+            except TypeError:
+                mensaje = "error" 
 
         datos.reverse()
         indice = len(datos)-1
-        variacion_eda = ((datos[indice]['enfermedades_diarreicas']-datos[0]['enfermedades_diarreicas'])/(datos[0]['enfermedades_diarreicas']))*100
-        variacion_ira = ((datos[indice]['enfermedades_respiratorias']-datos[0]['enfermedades_respiratorias'])/(datos[0]['enfermedades_respiratorias']))*100 
+        try:
+            variacion_eda = ((datos[indice]['enfermedades_diarreicas']-datos[0]['enfermedades_diarreicas'])/(datos[0]['enfermedades_diarreicas']))*100
+            variacion_ira = ((datos[indice]['enfermedades_respiratorias']-datos[0]['enfermedades_respiratorias'])/(datos[0]['enfermedades_respiratorias']))*100 
+        except :
+            variacion_eda = 0 
+            variacion_ira = 0
     variaciones = {'variacion_eda': variacion_eda, 'variacion_ira': variacion_ira}
     dicc = {'datos': datos, 'mensaje': mensaje, 'variaciones': variaciones, 'departamento': tiene_dep}
     return render_to_response('seguridad_alimentaria/utilizacion_biologica.html', dicc)
@@ -188,19 +206,22 @@ def disponibilidad(request, ano_inicial=None, ano_final=None, producto=None):
         disponibilidades.append(fila_disp)
         consumos.append(fila_consumo)
     else:
-        anos = DependenciaAlimentaria.objects.all().aggregate(minimo = Min('ano'), maximo= Max('ano'))
-        for ano in range(anos['minimo'], anos['maximo']+1):
-            poblacion = Poblacion.objects.get(ano=ano)
-            fila_disp = {'ano': ano, 'datos': []}
-            fila_consumo = {'ano': ano, 'datos': []}
-            for producto in productos:
-                dependencia = DependenciaAlimentaria.objects.get(ano=ano, producto=producto)
-                disponibilidad = dependencia.produccion + dependencia.importaciones + dependencia.donaciones - dependencia.exportaciones
-                consumo_aparente = disponibilidad/poblacion.total_ambos_sexos 
-                fila_disp['datos'].append(disponibilidad)
-                fila_consumo['datos'].append("%.2f" % consumo_aparente)
-            disponibilidades.append(fila_disp)
-            consumos.append(fila_consumo)
+        try:
+            anos = DependenciaAlimentaria.objects.all().aggregate(minimo = Min('ano'), maximo= Max('ano'))
+            for ano in range(anos['minimo'], anos['maximo']+1):
+                poblacion = Poblacion.objects.get(ano=ano)
+                fila_disp = {'ano': ano, 'datos': []}
+                fila_consumo = {'ano': ano, 'datos': []}
+                for producto in productos:
+                    dependencia = DependenciaAlimentaria.objects.get(ano=ano, producto=producto)
+                    disponibilidad = dependencia.produccion + dependencia.importaciones + dependencia.donaciones - dependencia.exportaciones
+                    consumo_aparente = disponibilidad/poblacion.total_ambos_sexos 
+                    fila_disp['datos'].append(disponibilidad)
+                    fila_consumo['datos'].append("%.2f" % consumo_aparente)
+                disponibilidades.append(fila_disp)
+                consumos.append(fila_consumo)
+        except: 
+            pass
 
     #variaciones!
     variacion_disp = []
