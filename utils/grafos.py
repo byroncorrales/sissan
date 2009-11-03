c2e88f8667939e34e52355cfb481869d413b3730
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2009-11-03 11:02:38 -0600

    fixing bugs

--- utils/grafos.py
+++ utils/grafos.py
@@ -1,12 +1,12 @@
-from pygooglechar import PieChart3D
+from pygooglechart import PieChart3D, PieChart2D
 from django.utils import simplejson
 from django.http import HttpResponse
 
-def make_graphic(data, legends, message=None, return_json = True):
-    graph = PieChart3D(600,350)
+def make_graphic(data, legends, message=None, return_json = True, type=PieChart3D):
+    graph = type(600,350)
     graph.set_colours([ 'FFBC13','22A410','E6EC23','2B2133','BD0915','3D43BD'])
     graph.add_data(data)
-    graph.set_legend(leyends)
+    graph.set_legend(legends)
     porcentajes = saca_porcentajes(data)
     graph.set_pie_labels(porcentajes)
     graph.set_legend_position("b")
@@ -23,6 +23,6 @@ def saca_porcentajes(values):
     total = sum(values)
     valores = [] 
     for i in range(len(values)):
-    porcentaje = (float(values[i])/total)*100
-    valores.append("%.2f" % porcentaje + '%')
+        porcentaje = (float(values[i])/total)*100
+        valores.append("%.2f" % porcentaje + '%')
     return valores
