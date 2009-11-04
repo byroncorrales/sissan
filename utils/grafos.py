9ca58019529b90d32e8241278cf1e8b6187b38f4
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2009-11-04 09:40:15 -0600

    fixing grafos.py

--- utils/grafos.py
+++ utils/grafos.py
@@ -1,15 +1,13 @@
-from pygooglechart import PieChart3D, PieChart2D
+from pygooglechart import PieChart3D, PieChart2D, StackedHorizontalBarChart, StackedVerticalBarChart
 from django.utils import simplejson
 from django.http import HttpResponse
 
-def make_graphic(data, legends, message=None, return_json = True, type=PieChart3D):
-    graph = type(600,350)
-    graph.set_colours([ 'FFBC13','22A410','E6EC23','2B2133','BD0915','3D43BD'])
-    graph.add_data(data)
-    graph.set_legend(legends)
-    porcentajes = saca_porcentajes(data)
-    graph.set_pie_labels(porcentajes)
-    graph.set_legend_position("b")
+def make_graphic(data, legends, message=None, return_json = True, type=PieChart3D, size=(600, 350)):
+    if (type==PieChart3D or type==PieChart2D):
+        graph = __pie_graphic__(data, legends, size, type)
+    elif (type==StackedHorizontalBarChart or type==StackedVerticalBarChart):
+        graph = __bar_graphic__(data, legends, type, size)
+
     graph.set_title(message)
 
     if return_json:
@@ -17,6 +15,37 @@ def make_graphic(data, legends, message=None, return_json = True, type=PieChart3
         return HttpResponse(simplejson.dumps(dict), mimetype='application/javascript')
     else:
         return graph.get_url()
+
+
+def __pie_graphic__(data, legends, size, type=PieChart3D):
+    graph = type(size[0], size[1])
+    graph.set_colours([ 'FFBC13','22A410','E6EC23','2B2133','BD0915','3D43BD'])
+    graph.add_data(data)
+    graph.set_legend(legends)
+    porcentajes = saca_porcentajes(data)
+    graph.set_pie_labels(porcentajes)
+    graph.set_legend_position("b")
+
+    return graph
+
+def __bar_graphic__(data, legends, size,  type=StackedVerticalBarChart):
+    #if type==StackedHorizontalBarChart:
+    #    graph = StackedHorizontalBarChart(size[0], size[1], x_range=range)
+    #else
+    #    graph = StackedVerticalBarChart(size[0], size[1], y_range=range)
+    
+    #graph.set_colours(['FF0000', '00FF00'])
+    #graph.set_bar_width(5)
+    #graph.set_bar_spacing(5)
+    #graph.set_legend(legends)
+
+    #for fila in data:
+    #    if (type(foo)==type([])):
+    #        graph.add_data(fila)
+    #    else:
+    #        #raise exception
+    #        pass
+    pass
 
 def saca_porcentajes(values):
     """sumamos los valores y devolvemos una lista con su porcentaje"""
